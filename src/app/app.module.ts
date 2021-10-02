e51b8c240f3d3c774e77521228d7f36bb694b842
Solofaxum <solofaxum6@gmail.com>
2021-10-02 15:06:01 -0700

    start up details updated/solomon

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeesComponent,
+    EmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
